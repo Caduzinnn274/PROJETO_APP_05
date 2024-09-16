264e8c0b875b4e976f17aa4587db8549a5e3266a
Carlos Bressan <105678823+Caduzinnn274@users.noreply.github.com>
2024-09-16 10:19:09 -0300

    Update script.js

--- Aula02_Function/script.js
+++ Aula02_Function/script.js
@@ -4,10 +4,50 @@
     function entrar()
     {
         var nome =prompt("Qual seu nome?");
-        area.innerHTML = "Bem vindo"+ nome+"" ;
 
+        if(nome === null || nome === "")
+        {
+            alert("insira um nome !!!!");
+            area.innerHTML = "Clique no botão para acessar"
+        }
+        else
+        {
+            area.innerHTML = "Bem vindo " + nome;
+            let botaoSair = document.createElement('button');
+            botaoSair.innerText = "Sair";
+            botaoSair.onclick = sair;
+            area.appendChild(botaoSair);
+ 
+        }
+       
 
-        let botaoSair = document.createElement('button');
+        
+    }
+
+    function sair()
+    {
+        alert("até mais nobre!");
+        area.innerHTML = "Você saiu";
     }
     
+
+    function mediaAluno(nota1, nota2)
+    {
+        var media = (nota1,nota2)/2;
+
+        if(media >=6)
+        {
+            console.log("Que belezura, você foi aprovado" + media);
+        }
+        else if(media <6)
+        {
+            console.log("Que vergonha, você foi reprovado" + media);
+        }
+    }
+
+    function aluno(nome,curso)
+    {
+        var mensagem = "Seja bem vindo " +nome+ "ao curso "+curso;
+        console.log(mensagem);
+    }
    
