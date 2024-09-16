61a5e5836c0d629b0865f5d58bcce1b432b86268
Carlos Bressan <105678823+Caduzinnn274@users.noreply.github.com>
2024-09-16 09:16:02 -0300

    Add files via upload

---
+++ Aula02_Function/script.js
@@ -0,0 +1,13 @@
+    var area = document.getElementById("area");
+    
+    
+    function entrar()
+    {
+        var nome =prompt("Qual seu nome?");
+        area.innerHTML = "Bem vindo"+ nome+"" ;
+
+
+        let botaoSair = document.createElement('button');
+    }
+    
+   
