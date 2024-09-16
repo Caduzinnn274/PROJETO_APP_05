7571681d932066d2725734e5b13844d55509bf47
Carlos Bressan <105678823+Caduzinnn274@users.noreply.github.com>
2024-09-16 10:51:49 -0300

    Add files via upload

---
+++ Aula03_Arrays/script.js
@@ -0,0 +1,26 @@
+var lista = ['aluno1', 'aluno2', 'aluno3', 100];
+console.log(lista);
+
+//descobrindo o tamanho do array
+
+console.log(lista.length);
+console.log(lista.indexOf('Aluno3'));
+console.log(lista.indexOf('Aluno4'));
+console.log(lista);
+
+
+lista[0] = 'Asdrubal';
+console.log(lista);
+
+lista.push('Aluno04');
+console.log(lista);
+
+lista.shift();
+console.log(lista);
+
+
+lista.pop();
+console.log(lista);
+
+lista.join(',');
+console.log(lista.join(','));
