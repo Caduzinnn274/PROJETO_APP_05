3386ba7403034633112278838a71ec691998d885
Carlos Bressan <105678823+Caduzinnn274@users.noreply.github.com>
2024-09-25 08:43:32 -0300

    Add files via upload

---
+++ Aula08_Variaveis/script.js
@@ -0,0 +1,27 @@
+var nome = "Adamastor";
+console.log(nome);
+
+if(nome == "Adamastor")
+{
+   var escola = "CESF";
+   console.log(escola);
+}
+console.log(escola);
+escola = "Sagrada ";
+console.log(escola);
+
+//let
+if(nome === "Adamastor")
+{
+    let curso = "DS";
+    console.log(curso);
+}
+
+curso = "culinaria";
+
+console.log(curso);
+
+//const
+
+const cargo = 'estudante'
+console.log(cargo)
