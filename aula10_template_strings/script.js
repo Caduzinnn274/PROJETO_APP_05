ec9053244a12bf9c0a48b61359b2d411c7285c13
Carlos Bressan <105678823+Caduzinnn274@users.noreply.github.com>
2024-09-27 12:04:08 -0300

    Add files via upload

---
+++ aula10_template_strings/script.js
@@ -0,0 +1,8 @@
+let nome = "Marcelo";
+let sobrenome = "Rubens";
+let idade = 30;
+
+console.log("meu nome é", nome, " ", sobrenome, "e minha idade é ", idade,);
+
+let mensagem = `Meu nome é ${nome} ${sobrenome} e minha idade é ${idade} anos `;
+console.log(mensagem);
