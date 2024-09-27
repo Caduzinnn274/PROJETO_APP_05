aea03bedba52d0fc7f638b4815a3786a90874510
Carlos Bressan <105678823+Caduzinnn274@users.noreply.github.com>
2024-09-27 11:45:59 -0300

    Add files via upload

---
+++ aula09_objetos/script.js
@@ -0,0 +1,29 @@
+let pessoa = {
+    nome: 'Marcelo',
+    sobrenome: 'Rubens',
+    idade: 54,
+    altura: 1.74,
+    cargo: 'Professor',
+}
+
+console.log(pessoa);
+console.log(pessoa.nome);
+console.log(pessoa.altura);
+
+
+let carro = {
+    nome: 'Brasilia',
+    cor: 'amarelo',
+    potencia:'80cv',
+}
+
+console.log(carro.nome + " " +carro.cor);
+
+
+let ususarios = [
+    {nome: 'Marcelo',cargo: 'Programador', status: 'Ativo'},
+    {nome: 'Bilbonei',cargo: 'engenheiro de software', status: 'Ativo'},
+    {nome: 'Conegundes',cargo: 'estagiário', status: 'inativo'},
+]
+console.log(ususarios);
+console.log([2].nome);
