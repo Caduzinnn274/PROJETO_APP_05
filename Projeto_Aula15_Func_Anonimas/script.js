4b79ac82b956173dc0a36f821228d2d0030241bf
Carlos Bressan <105678823+Caduzinnn274@users.noreply.github.com>
2024-10-07 09:41:56 -0300

    Add files via upload

---
+++ Projeto_Aula15_Func_Anonimas/script.js
@@ -0,0 +1,22 @@
+function Somar(a,b){
+    var total = a+b;
+    console.log(total);
+}
+Somar(2,10);
+
+
+
+let Subtrair = (numero1, numero2)=>{
+
+    var total = numero1-numero2;
+    console.log(total);
+}
+
+Subtrair(15,17);
+
+let Numeros = [1,2,3,10]
+{
+    Numeros.map((item)=>{
+        console.log(item);
+    })
+}
